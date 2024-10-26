6867f672680cc51150f0078ccde1f2fec40eb943
danaigc <167301973+danaigc@users.noreply.github.com>
2024-10-26 23:59:45 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center w-full">
       <HeroSection />
+
       <MusicShowcase />
       <TestimonialsSection />
       <PricingSection />
